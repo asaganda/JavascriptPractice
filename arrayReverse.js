799daaa5520588994026eead4bc8170882f9d285
Andrew <Asaganda@gmail.com>
2023-09-10 14:36:19 -0400

    solve array reverse

---
+++ arrayReverse.js
@@ -0,0 +1,10 @@
+// Array.reverse
+// In this coding challenge, you will be implementing a function called reverseArray(array, value) that takes one argument: an array. The function should reverse the order of the elements in the array.
+
+// Directions
+// If the input array is empty, return an empty array.
+
+export const reverseArray = (array) => {
+    // Write your code here
+    return array.reverse()
+}
