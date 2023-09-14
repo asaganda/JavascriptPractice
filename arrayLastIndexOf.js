811188b15727c20cbca1f97f72a6027fa91269fa
Andrew <Asaganda@gmail.com>
2023-09-14 17:59:39 -0400

    solve array last index of value

---
+++ arrayLastIndexOf.js
@@ -0,0 +1,10 @@
+// Array.lastIndexOf
+// In this coding challenge, you will be implementing a function called findLastIndexOf(array, value) that takes two arguments: an array and a value. The function should return the index of the last occurrence of the value in the array.
+
+// Directions
+// If no element is found, return -1.
+
+export const findLastIndexOf = (array, value) => {
+    // Write your code here
+    return array.lastIndexOf(value)
+}
